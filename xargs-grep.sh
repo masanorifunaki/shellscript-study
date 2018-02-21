e56e1e37b6d3a4eac9f83d9609006f8ff56cb084
masanorifunaki <funaki@img.co.jp>
2018-02-21 12:11:55 +0900

    3章032の実装

--- xargs-grep.sh
+++ xargs-grep.sh
@@ -8,4 +8,8 @@ readonly SCRIPT_NAME=${0##*/}
 dir="$1"
 
 # 拡張子.shのファイルから、"bin"という文字列を検索
+# 対象のファイルに/dev/nullを加えることで、grepコマンドの出力に必ず
+# ファイル名を含むようにするための処理になる。
+# grepコマンドでは複数のファイルを対象とした場合、先頭にファイル名をつけて
+# マッチした行を出力する。
 find "$dir" -name "*.sh" -print | xargs grep "bin" /dev/null #1
